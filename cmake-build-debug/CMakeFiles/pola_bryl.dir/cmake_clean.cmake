859eb09c8df9be169f1a429ebaf57e961aab8481
Kacper Olszewski <kacperolszewski00@gmail.com>
2020-04-01 16:42:24 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/pola_bryl.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pola_bryl.dir/main.cpp.obj"
+  "libpola_bryl.dll.a"
+  "pola_bryl.exe"
+  "pola_bryl.exe.manifest"
+  "pola_bryl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pola_bryl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/pola_bryl.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for pola_bryl.
+# This may be replaced when dependencies are built.
